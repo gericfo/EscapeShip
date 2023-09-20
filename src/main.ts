b3913ee791450833fc775f7c9e8cb6e7f2d60645
GericFo <52211274+gericfo@users.noreply.github.com>
2023-09-20 08:42:01 +0200

    script up

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,120 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
+const buttons = {
+    "button1": { x: 3, y: 5, color: 'red', pressed: false },
+    "button2": { x: 5, y: 5, color: 'red', pressed: false },
+    "button3": { x: 17, y: 5, color: 'blue', pressed: false },
+    "button4": { x: 19, y: 5, color: 'blue', pressed: false },
 
-// Waiting for the API to be ready
-WA.onInit().then(() => {
+}
+
+interface TileDescriptor {
+    x: number;
+    y: number;
+    tile: number | string | null;
+    layer: string;
+}
+
+// Attendons l'initialisation de l'API
+WA.onInit().then(async () => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    // Afin de pouvoir lire les variables des autres joueurs, nous devons activer le tracking des joueurs.
+    await WA.players.configureTracking({
+        players: true,
+    });
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    for (const [btnName, {x, y}] of Object.entries(buttons)) {
+        WA.room.area.create({
+            name: btnName,
+            x: x * 32,
+            y: y * 32,
+            width: 32,
+            height: 32,
+        });
+        WA.room.area.onEnter(btnName).subscribe(() => {
+            WA.player.state.saveVariable("buttonPressed", btnName, {
+                persist: false,
+                public: true,
+            });
+            updateMap();
+        });
+        WA.room.area.onLeave(btnName).subscribe(() => {
+            WA.player.state.saveVariable("buttonPressed", undefined, {
+                persist: false,
+                public: true,
+            });
+            updateMap();
+        });
+    }
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.players.onPlayerEnters.subscribe((player) => {
+        player.state.onVariableChange('buttonPressed').subscribe(() => {
+            updateMap();
+        });
+    });
+    WA.players.onPlayerLeaves.subscribe(() => {
+        updateMap();
+    });
 
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+    /*WA.players.onVariableChange('buttonPressed').subscribe(({player, value}) => {
+        console.log("VARIABLE CHANGED", value, player.state.buttonPressed);
+        updateMap();
+    });*/
+
+    updateMap();
 
 }).catch(e => console.error(e));
 
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
+function updateMap() {
+    // Reset pressed state
+    for (const button of Object.values(buttons)) {
+        button.pressed = false;
     }
+
+    let nbRedPressed = 0;
+    let nbBluePressed = 0;
+
+    const players = [...WA.players.list(), WA.player];
+    for (const player of players) {
+        const buttonPressed = player.state.buttonPressed;
+        // Workaround for Typescript type narrowing bug with "buttonPressed in buttons"
+        if (typeof buttonPressed === 'string' && (buttonPressed === "button1" || buttonPressed === "button2" || buttonPressed === "button3" || buttonPressed === "button4" || buttonPressed === "button5" || buttonPressed === "button6" || buttonPressed === "button7" || buttonPressed === "button8")) {
+            buttons[buttonPressed].pressed = true;
+            if (buttons[buttonPressed].color === 'red') {
+                nbRedPressed++;
+            }
+            if (buttons[buttonPressed].color === 'blue') {
+                nbBluePressed++;
+            }
+        }
+    }
+
+    displayButtons();
+
+    if (nbRedPressed >= 2) {
+        WA.room.showLayer('doors/red_door_opened');
+        WA.room.hideLayer('doors/red_door_closed');
+    } else {
+        WA.room.hideLayer('doors/red_door_opened');
+        WA.room.showLayer('doors/red_door_closed');
+    }
+    if (nbBluePressed >= 2) {
+        WA.room.showLayer('doors/blue_door_opened');
+        WA.room.hideLayer('doors/blue_door_closed');
+    } else {
+        WA.room.hideLayer('doors/blue_door_opened');
+        WA.room.showLayer('doors/blue_door_closed');
+    }
+}
+
+function displayButtons() {
+    let tiles: TileDescriptor[] = [];
+    for (const {x, y, color, pressed} of Object.values(buttons)) {
+        tiles.push({ x, y, tile: color + (pressed ? '_enabled' : '_disabled'), layer: 'buttons' });
+    }
+    WA.room.setTiles(tiles);
 }
 
 export {};
